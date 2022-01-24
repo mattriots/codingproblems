959fa34d5795e8923f5a9014b2adb8c68820d5fb
mattriots <mattdepauw@gmail.com>
2022-01-23 19:47:52 -0800

    First commit

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,50 @@
+//import java.util.*;
+//
+//public class AddTwoNumbers {
+//
+//    public static void main(String[] args) {
+//
+//        Solution sol = new Solution();
+//        sol.letterCombinations("23");
+//    }
+//
+//    static class Solution {
+//        public List<String> letterCombinations(String digits) {
+//            List<String> result = new ArrayList<>();
+//
+//            Map<Character, List<String>> current = new HashMap<>();
+//            List<List<String>> currentList = new ArrayList<>();
+//
+//            Map<Character, List<String>> numberToLetters = new HashMap<>();
+//            numberToLetters.put('2', Arrays.asList("a", "b", "c"));
+//            numberToLetters.put('3', Arrays.asList("d", "e", "f"));
+//            numberToLetters.put('4', Arrays.asList("g", "h", "i"));
+//            numberToLetters.put('5', Arrays.asList("j", "k", "l"));
+//            numberToLetters.put('6', Arrays.asList("m", "n", "o"));
+//            numberToLetters.put('7', Arrays.asList("p", "q", "r", "s"));
+//            numberToLetters.put('8', Arrays.asList("t", "u", "v"));
+//            numberToLetters.put('9', Arrays.asList("w", "x", "y", "z"));
+//
+//
+//            for (int i = 0; i < digits.length(); i++) {
+//               currentList.add(numberToLetters.get(digits.charAt(i)));
+//            }
+//
+////            System.out.println(currentList);
+//            combinations(currentList);
+//
+//            return result;
+//        }
+//    }
+//
+//    public static List<String> combinations(List<List<String>> letters){
+//
+//        for(List lst : letters){
+//            for (Object o : lst) {
+//                System.out.println(o);
+//            }
+//        }
+//
+//        return null;
+//    }
+//}
---
+++ src/SortArrays.java
@@ -0,0 +1,34 @@
+//import java.util.Arrays;
+//
+//public class SortArrays {
+//
+//    public static void main(String[] args) {
+////        int[] nums1 = {1,2,3,0,0,0};
+////        int[] nums2 = {2,5,6};
+////        int m = 3;
+////        int n = 3;
+//
+//        int[] nums1 = {0,0,0,0,0};
+//        int[] nums2 = {1,2,3,4,5};
+//        int m = 0;
+//        int n = 5;
+//
+//        Solution sol = new Solution();
+//        sol.merge(nums1, m, nums2, n);
+//
+//    }
+//
+//     static class Solution {
+//        public void merge(int[] nums1, int m, int[] nums2, int n) {
+//
+//           for(int i = 0; i < nums2.length; i++){
+//               nums1[m + i] = nums2[i];
+//            }
+//
+//            Arrays.sort(nums1);
+//            for(Integer num : nums1){
+//                System.out.println(num);
+//            }
+//        }
+//    }
+//}
