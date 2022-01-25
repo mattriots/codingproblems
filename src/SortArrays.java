5561eb8cc82434cadcc10731aaf2ed18f2681a41
mattriots <mattdepauw@gmail.com>
2022-01-24 19:57:22 -0800

    changed somethings

--- src/SortArrays.java
+++ src/SortArrays.java
@@ -3,15 +3,15 @@ import java.util.Arrays;
 public class SortArrays {
 
    public static void main(String[] args) {
-//        int[] nums1 = {1,2,3,0,0,0};
-//        int[] nums2 = {2,5,6};
-//        int m = 3;
-//        int n = 3;
+       int[] nums1 = {1,2,3,0,0,0};
+       int[] nums2 = {2,5,6};
+       int m = 3;
+       int n = 3;
 
-       int[] nums1 = {0,0,0,0,0};
-       int[] nums2 = {1,2,3,4,5};
-       int m = 0;
-       int n = 5;
+    //    int[] nums1 = {0,0,0,0,0};
+    //    int[] nums2 = {1,2,3,4,5};
+    //    int m = 0;
+    //    int n = 5;
 
        Solution sol = new Solution();
        sol.merge(nums1, m, nums2, n);
